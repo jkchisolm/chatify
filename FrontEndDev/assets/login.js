05094bd790c9095554b825f40f5e486517f6bcd7
Joshua Chisolm <jkchisolm22@gmail.com>
2022-12-01 11:52:44 -0800

    make dynamic web project

--- FrontEndDev/assets/login.js
+++ FrontEndDev/assets/login.js
@@ -1,4 +1,4 @@
-const loginUser = () => {
+const loginUser = async () => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
@@ -7,10 +7,28 @@ const loginUser = () => {
     alert("Please enter a username and password");
     return;
   }
-  alert(
-    "Logged in user (placeholder). Will redirect to login page 5 seconds after clicking OK."
+  // try making GET request to servlet with username and password?
+  // if successful, return response in an alert
+
+  //fetch
+  const response = await fetch(
+    "http://localhost:8080/test-stuff/myForm?user=" +
+      username +
+      "&pass=" +
+      password,
+    { method: "GET", mode: "*cors" }
   );
-  setTimeout(() => {
-    window.location.href = "countdown.html";
-  }, 5000);
+  console.log(response);
+  alert("Check console");
+  // const responseText = await response.text();
+  // alert(responseText);
+
+  // alert(
+  //   "Logged in user (placeholder). Will redirect to login page 1 seconds after clicking OK."
+  // );
+  // setTimeout(() => {
+  //   alert("Redirecting to spotify page to log in user");
+  //   requestAuthorization();
+  //   // window.location.href = "countdown.html";
+  // }, 1000);
 };
